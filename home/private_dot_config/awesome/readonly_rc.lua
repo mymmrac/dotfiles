3c9422314d0dc291eaeacf8f2b6aadcdfab6d0d6
Artem Yadelskyi <mymmrac@gmail.com>
2023-04-09 21:16:30 +0300

    :deciduous_tree: Updated awesome & polybar configs

--- home/private_dot_config/awesome/readonly_rc.lua
+++ home/private_dot_config/awesome/readonly_rc.lua
@@ -49,6 +49,7 @@ end
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 beautiful.wallpaper = gears.filesystem.get_configuration_dir() .. "image/bg.jpg"
+beautiful.useless_gap = 3
 
 -- This is used later as the default terminal and editor to run.
 terminal = "alacritty"
@@ -66,17 +67,17 @@ mod_key = "Mod4"
 awful.layout.layouts = {
     awful.layout.suit.floating,
     awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier,
-    awful.layout.suit.corner.nw,
+    --awful.layout.suit.tile.left,
+    --awful.layout.suit.tile.bottom,
+    --awful.layout.suit.tile.top,
+    --awful.layout.suit.fair,
+    --awful.layout.suit.fair.horizontal,
+    --awful.layout.suit.spiral,
+    --awful.layout.suit.spiral.dwindle,
+    --awful.layout.suit.max,
+    --awful.layout.suit.max.fullscreen,
+    --awful.layout.suit.magnifier,
+    --awful.layout.suit.corner.nw,
 }
 -- }}}
 
